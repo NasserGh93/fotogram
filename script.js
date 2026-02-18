04588fc0cfeb0fa67503202b08b57992aeab68ce
NasserGh93 <nasserghaibeh@googlemail.com>
2026-02-18 07:16:33 +0100

    komments raus genommen

--- script.js
+++ script.js
@@ -11,27 +11,12 @@ const myImg = [
 const imgContainer = document.querySelector(".img-container");
 const dialog = document.getElementById("my-dialog");
 
-
-// Das Bild, das im Dialog angezeigt wird
 const nextBtn = document.getElementById("next");
 const dialogImg = document.getElementById("dialog-image");
 nextBtn.before(dialogImg);
 
-dialog.addEventListener("click", (e) => {
-  const rect = dialog.getBoundingClientRect();
 
-  const isInDialog =
-    e.clientX >= rect.left &&
-    e.clientX <= rect.right &&
-    e.clientY >= rect.top &&
-    e.clientY <= rect.bottom;
 
-  //if (!isInDialog) {
-  //closeDialog();
-  //}
-});
-
-let currentIndex = 0;
 
 function showDialogImage(index) {
   currentIndex = index;
@@ -44,10 +29,9 @@ function showDialogImage(index) {
   dialogCaption.innerHTML = `<button  class="btn-responsiv" onclick="prev()"><img src="./img/prev.png" alt="Preview"></button>Image: ${currentIndex + 1} of ${myImg.length} <button class="btn-responsiv" onclick="next()"><img src="./img/next.png" alt="Next"></button>`;
 }
 
-
-//
 myImg.forEach((src, i) => {
   const img = document.createElement("img");
+  img.ariaLabel = document.getElementsByName("myImg");
   img.src = src;
   img.alt = src.split("/").pop();
   img.loading = "lazy";
@@ -56,23 +40,19 @@ myImg.forEach((src, i) => {
   img.tabIndex = 0;
   img.style.cursor = "pointer";
 
-  // Enter / Space öffnet den Dialog (wie ein Button):
   img.addEventListener("keydown", (e) => {
     if (e.key === "Enter" || e.key === " ") {
       e.preventDefault();
       showDialogImage(i);
       openDialog();
     }
+
   });
 
-  // Maus klick bleibt:
   img.addEventListener("click", () => {
     showDialogImage(i);
     openDialog();
   });
-
-
-
   imgContainer.appendChild(img);
 
 });
@@ -81,7 +61,6 @@ dialog.addEventListener("keydown", (e) => {
   if (e.key === "ArrowLeft") prev();
   if (e.key === "ArrowRight") next();
 });
-// Text unter dem Bild (Bildname)
 const dialogCaption = document.createElement("p");
 dialogCaption.id = "dialog-caption";
 dialogCaption.style.textAlign = "center";
@@ -99,8 +78,6 @@ function closeDialog() {
   dialog.close();
   dialog.classList.remove("opend");
 }
-
-// funktionen um nächstes oder Vorheriges Bild zu sehen
 
 
 
